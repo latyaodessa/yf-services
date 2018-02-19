47c27f8690663e3359ffb431b8ea069d53141f81
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2018-02-19 18:26:19 +0100

    scheduler

--- src/yf/post/parser/rest/scheduler/RestScheduler.java
+++ src/yf/post/parser/rest/scheduler/RestScheduler.java
@@ -1,41 +1,27 @@
 package yf.post.parser.rest.scheduler;
 
-import java.util.logging.Logger;
-
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Timeout;
 import javax.inject.Inject;
 
-import yf.post.parser.ParserService;
-import yf.post.parser.rest.client.ParserRestClient;
-
 @Singleton
-public class RestScheduler implements Scheduler{
-
-	@Inject
-	ParserRestClient parserRestClient;
-	@Inject
- 	ParserService parserService;
-    private Logger logger = Logger.getLogger(RestScheduler.class.getName());
-    
+public class RestScheduler implements Scheduler {
 
 
-	
+    @Inject
+    private SchedulerProcessor processor;
+
+
     @Schedule(second = "05", minute = "01,03,05", hour = "*")
-	@Timeout
-	public void process() {
+    @Timeout
+    public void process() {
 //    		parserRestClient.checkNewPosts();
-    	
-    	try{
-    		 parserService.triggerPostParserForNewPosts();
-     		logger.info("New Posts Rest Call is triggered");
-    	} catch (Exception e){
-     		logger.severe("New Posts Rest Call EXCEPTION! resexp" + e);
-    	  }
-    		
-			
-		
-	}
+
+
+        processor.triggerVkScraper();
+
+
+    }
 
 }
